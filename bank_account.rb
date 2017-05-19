9dd9065b038312f0f39a82d12e961a4b4199fac7
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-19 14:58:14 -0400

    Add comments to Bank Acct. Program

--- bank_account.rb
+++ bank_account.rb
@@ -1,21 +1,28 @@
+# Creat Class Bank Account
+
 class BankAccount
 
+# Read and Writable balance and interest rate
   attr_accessor :balance, :interest_rate
 
+# Initialize method for class with default variable values
   def initialize
     @balance = 0.00
     @interest_rate = 0.04
   end
 
+# Method to Deposit $$
   def deposit(amount)
     @balance += amount
   end
 
+# Method to Withdraw $$
   def withdraw(amount)
     @balance -= amount
   end
 
-  def gain_inerest
+# Method to Apply Interest to Current Balance
+  def gain_interest
     @balance += @balance * @interest_rate
   end
 
