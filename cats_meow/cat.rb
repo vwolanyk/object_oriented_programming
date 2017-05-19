a11f639c44bdb33d100a25a6d9de5c110dbc4531
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-19 16:09:27 -0400

    Add method to display meal time for cat - works

--- cats_meow/cat.rb
+++ cats_meow/cat.rb
@@ -6,6 +6,24 @@ class Cat
     @meal_time        = meal_time
   end
 
+# 2 Instances of Cat
+fido = Cat.new("fido","Alpo", 5)
+yarnball = Cat.new("yarnball", "caviar", 3)
+
+# Method to Display a cat's meal time
+
+  def eats_at
+    if @meal_time == 12
+      "12 PM"
+    elsif @meal_time == 0
+      "12 AM"
+    elsif @meal_time > 12
+      "#{@meal_time - 12} PM"
+    else @meal_time < 12
+      "#{@meal_time} AM"
+    end
+
+  end
 
 
 end
