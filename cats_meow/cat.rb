5f53e1a77e944d70e837b8b7f987f820bb5e76e5
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-19 15:16:09 -0400

    Create Initialize method(name, preferred food, meal time) - Works

--- cats_meow/cat.rb
+++ cats_meow/cat.rb
@@ -1,5 +1,11 @@
 class Cat
 
+  def initialize(name,preferred_food,meal_time)
+    @name             = name
+    @preferred_food   = preferred_food
+    @meal_time        = meal_time
+  end
+
 
 
 end
