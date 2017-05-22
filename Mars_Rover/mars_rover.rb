aeb009a94827d520b5da2ed59f073f1a78b87cda
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-22 19:36:00 -0400

    Creat Check for valid instruction input, method to read instructions - works

--- Mars_Rover/mars_rover.rb
+++ Mars_Rover/mars_rover.rb
@@ -1,13 +1,41 @@
+# MARS ROVER ASSIGNMENT
+
+# Class Rover
 class Rover
 
+  # Init Method takes x,y and direction coordinates
   def initialize(x_coord,y_coord,direction)
     @x_coord    = x_coord
     @y_coord    = y_coord
     @direction  = direction
   end
 
+  #Instruction Method
+  def read_instructions(instruction)
+    # String to Array of Instructions
+    # Convert to Upcase Format for reading consistency
+    instructions_list = instruction.upcase!.split(//)
+
+
+    # Check to see if Valid Input
+    instructions_list.each do |instruction|
+       unless (instruction == "M") || (instruction == "L") || (instruction == "R")
+         puts "Invalid Entry"
+         return "Invalid Entry"
+       end
+      end
+
+    # Evaluate Input Deploy Methods 
+    instructions_list.each do |move_value|
+      if move_value == "M"
+          puts "move"
+      else puts "turn #{move_value}"
+          end
+        end
+    end
 
 end
-
 come_over = Rover.new(3, 7, "N")
-puts come_over
+
+
+come_over.read_instructions("mmlrrmm")
