fa25eab50ede521deeef2a48961f059d67cc6cf6
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-22 19:53:59 -0400

    Create Move Method, Create Position Method - tested

--- Mars_Rover/mars_rover.rb
+++ Mars_Rover/mars_rover.rb
@@ -3,11 +3,19 @@
 # Class Rover
 class Rover
 
+  attr_reader :x_coord, :y_coord, :direction
+
   # Init Method takes x,y and direction coordinates
   def initialize(x_coord,y_coord,direction)
     @x_coord    = x_coord
     @y_coord    = y_coord
-    @direction  = direction
+    @direction  = direction.upcase
+  end
+
+  def position
+    puts "X: #{@x_coord}"
+    puts "Y: #{@y_coord}"
+    puts "Direction: #{@direction}"
   end
 
   #Instruction Method
@@ -15,6 +23,19 @@ class Rover
     # String to Array of Instructions
     # Convert to Upcase Format for reading consistency
     instructions_list = instruction.upcase!.split(//)
+
+    def move
+      case @direction
+      when "N"
+        @y_coord += 1
+      when "S"
+        @y_coord -= 1
+      when "W"
+        @x_coord += 1
+      when "E"
+        @x_coord -= 1
+      end
+    end
 
 
     # Check to see if Valid Input
@@ -25,10 +46,10 @@ class Rover
        end
       end
 
-    # Evaluate Input Deploy Methods 
+    # Evaluate Input Deploy Methods
     instructions_list.each do |move_value|
       if move_value == "M"
-          puts "move"
+          move
       else puts "turn #{move_value}"
           end
         end
@@ -36,6 +57,8 @@ class Rover
 
 end
 come_over = Rover.new(3, 7, "N")
-
+come_over.position
 
 come_over.read_instructions("mmlrrmm")
+
+come_over.position
