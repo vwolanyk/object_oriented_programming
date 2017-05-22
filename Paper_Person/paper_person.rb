feeeb86a38aede041f6952771800f0ffbc026701
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-22 14:47:46 -0400

    Create Deliver Method - Increases Earnings and Experience

--- Paper_Person/paper_person.rb
+++ Paper_Person/paper_person.rb
@@ -11,6 +11,7 @@ attr_reader :earnings
     @name       = name
     @experience = 0
     @quota      = 50
+    @earnings   = 0.00
   end
 
 # Quota Metod - Determines current quota
@@ -18,10 +19,32 @@ attr_reader :earnings
     @quota = (@experience / 2) + 50
   end
 
+
+  def deliver(start_address, end_address)
+    #Calculates Number of Houses Delivered regardless of Start and End Value
+     delivery_total = (start_address - end_address).abs
+
+     # Calculates Earnings Based on Delivery
+     if delivery_total < 50
+       @earnings = (delivery_total.to_f * 0.25) - 2.00
+     else above_quota = (delivery_total.to_f - 50)  * 0.50
+       @earnings += above_quota + 12.50
+     end
+
+    # Increases Experience based on delivery total
+    @experience += delivery_total
+
+  end
+
+
 end
 
 # Test Case
 # ahmed = PaperPerson.new("Ahmed")
 # puts ahmed.name
+# puts ahmed.quota
+# puts ahmed.experience
+# puts ahmed.deliver(400,232)
 # puts ahmed.experience
 # puts ahmed.quota
+# puts ahmed.earnings
