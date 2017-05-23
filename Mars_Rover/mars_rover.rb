41024f77357f9bab77ca598def98fb04a03895bf
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-22 20:36:35 -0400

    Create Turn Method - tested, works

--- Mars_Rover/mars_rover.rb
+++ Mars_Rover/mars_rover.rb
@@ -24,6 +24,7 @@ class Rover
     # Convert to Upcase Format for reading consistency
     instructions_list = instruction.upcase!.split(//)
 
+    # Method to Move Rover
     def move
       case @direction
       when "N"
@@ -37,11 +38,39 @@ class Rover
       end
     end
 
+  # Method to Turn Rover Left or Right
+  def turn(l_or_r)
+    if  l_or_r == "L"
+      case @direction
+      when "N"
+        @direction = "W"
+      when "E"
+        @direction = "N"
+      when "S"
+        @direction = "E"
+      when "W"
+        @direction = "S"
+      end
+    else
+      case @direction
+      when "N"
+        @direction = "E"
+      when "E"
+        @direction = "S"
+      when "S"
+        @direction = "W"
+      when "W"
+        @direction = "N"
+      end
+    end
+  end
+
 
     # Check to see if Valid Input
     instructions_list.each do |instruction|
        unless (instruction == "M") || (instruction == "L") || (instruction == "R")
-         puts "Invalid Entry"
+         puts "Houston: We have a Problem!"
+         puts "Re-Enter Commands"
          return "Invalid Entry"
        end
       end
@@ -50,15 +79,18 @@ class Rover
     instructions_list.each do |move_value|
       if move_value == "M"
           move
-      else puts "turn #{move_value}"
+      else turn(move_value)
           end
         end
     end
 
 end
-come_over = Rover.new(3, 7, "N")
-come_over.position
 
-come_over.read_instructions("mmlrrmm")
+come_over_red = Rover.new(3, 7, "N")
+come_over_red.position
 
-come_over.position
+come_over_red.read_instructions("MmEmlrsmm")
+
+come_over_red.read_instructions("mmlRrMm")
+
+come_over_red.position
